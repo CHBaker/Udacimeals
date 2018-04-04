1efb28f63999d6e3dd86db8591a4d4d2b7441e4f
CHBaker <charlesprobaker@mail.com>
2018-04-03 21:16:08 -0600

    FEATURE+STATE: connect component to state
    - pass state as props object

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
-
     render() {
+        console.log(this.props);
         return (
             <div>
                 hello world
@@ -13,4 +14,18 @@ class App extends Component {
     }
 }
 
-export default App;
+function mapStateToProps (calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    return {
+        calendar: dayOrder.map(day => ({
+            day: day,
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals[meal] = calendar[day][meal]
+                    ? calendar[meals][meal] : null
+                return meals;
+            }, {})
+        }))
+    }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -15,5 +15,6 @@ const store = createStore(
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, document.getElementById('root'));
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
