0713ebef15790c48217f6d3a98dea5004851bb32
CHBaker <charlesprobaker@mail.com>
2018-04-08 18:40:55 -0600

    FEATURE: add food modal UI
    - Uses react icons
    - Register API keys for food search
    - Add local state for modal

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,97 @@ import logo from '../logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { addRecipe, removeFromCalendar } from '../actions'
+import { capitalize } from '../utils/helper';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
+import Modal from 'react-modal';
+import RightArrowIcon from 'react-icons/lib/fa/arrow-circle-right';
+import Loading from 'react-loading';
+import { fetchRecipes } from '../utils/api';
+import FoodList from './FoodList';
 
 class App extends Component {
-    doThing = () => {
-        this.props.addRecipe({})
+    state = {
+        foodModalOpen: false,
+        meal: null,
+        day: null,
+        food: null,
+        loadingFood: false,
     }
+
+    openFoodModal = ({ meal, day }) => {
+        this.setState(() => ({
+            foodModalOpen: true,
+            meal,
+            day,
+        }))
+    }
+    closeFoodModal = () => {
+        this.setState(() => ({
+            foodModalOpen: false,
+            meal: null,
+            day: null,
+            food: null,
+        }))
+    }
+    searchFood = (e) => {
+        if (!this.input.value) {
+            return
+        }
+
+        e.preventDefault()
+
+        this.setState(() => ({ loadingFood: true }))
+
+        fetchRecipes(this.input.value)
+            .then((food) => this.setState(() => ({
+                food,
+                loadingFood: false,
+            })))
+    }
+
     render() {
-        console.log(this.props);
+        const { calendar, remove } = this.props;
+        const mealOrder = ['breakfast', 'lunch', 'dinner'];
+
         return (
-            <div>
-                hello world
+            <div className='container'>
+                <ul className='meal-types'>
+                    {mealOrder.map((mealType) => (
+                        <li key={mealType} className='subheader'>
+                            {capitalize(mealType)}
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='calendar'>
+                    <div className='days'>
+                        {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+                    </div>
+                    <div className='icon-grid'>
+                        {calendar.map(({ day, meals }) => (
+                            <ul key={day}>
+                                {mealOrder.map((meal) => (
+                                    <li key={meal} className='meal'>
+                                        {meals[meal]
+                                            ? <div className='food-item'>
+                                                <img src={meals[meal].image} alt={meals[meal].label} />
+                                                <button onClick={() => remove({ meal, day })}>Clear</button>
+                                            </div>
+                                            : <button className='icon-btn'>
+                                                <CalendarIcon size={30} />
+                                            </button>}
+                                    </li>
+                                ))}
+                            </ul>
+                        ))}
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-function mapStateToProps ({ calendar, food }) {
+function mapStateToProps({ calendar, food }) {
     const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
     return {
         calendar: dayOrder.map(day => ({
